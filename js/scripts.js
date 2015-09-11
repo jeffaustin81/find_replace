8b4a1b4b82965fedf2218dc6ccbcfccb305413e0
Jeff Austin <jeffaustin81@gmail.com>
2015-09-11 13:38:13 -0700

    Formatted html, and js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
 var findReplace = function(message, find, replace) {
-	var re = "\\b" + find + "\\b";
-	var reg = new RegExp(re, "gi")
-	var new_message = message.replace(reg, replace); 
-	return new_message;
+    var re = "\\b" + find + "\\b";
+    var reg = new RegExp(re, "gi")
+    var new_message = message.replace(reg, replace);
+    return new_message;
 };
 
-$(document).ready(function () {
-	$("form#add_message").submit(function(event) {
+$(document).ready(function() {
+    $("form#add_message").submit(function(event) {
 
-		var message = $("input#message").val();
-		
-		$(".message").text(message);
+        var message = $("input#message").val();
 
-		$("#original_message").show();
-		$("form#add_message").hide();
-		$("form#find_replace").show();
-		event.preventDefault();
-		
-		$("form#find_replace").submit(function(event) {
+        $(".message").text(message);
 
-		var find = $("input#find").val();
-		var replace = $("input#replace").val();
-		var replace_message = findReplace(message, find, replace);
+        $("#original_message").show();
+        $("form#add_message").hide();
+        $("form#find_replace").show();
+        event.preventDefault();
+
+        $("form#find_replace").submit(function(event) {
+
+            var find = $("input#find").val();
+            var replace = $("input#replace").val();
+            var replace_message = findReplace(message, find, replace);
 
 
-		$(".new_message").text(replace_message);
+            $(".new_message").text(replace_message);
 
-		$("#new_message").show();
-		$("#original_message").hide();
-		$("form#find_replace").hide();
-		$("form#home").show();
-		event.preventDefault();
-	    });
-	});
-	
-	
+            $("#new_message").show();
+            $("#original_message").hide();
+            $("form#find_replace").hide();
+            $("form#home").show();
+            event.preventDefault();
+        });
+    });
+
+
 });
